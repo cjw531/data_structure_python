e9ae355d151aad13372ce3337b19c88971f6e658
Jiwon Choi <jiwoncho@buffalo.edu>
2020-08-28 03:05:21 +0900

    implemented binary tree traversal in python, with stack-based approach; bug fixed in recurisve approach (root node's depth is 0); added basic tests

--- tree_traversal.py
+++ tree_traversal.py
@@ -1,3 +1,5 @@
+from stack import *
+
 class TreeNode:
     def __init__ (self, value):
         self.value = value
@@ -12,12 +14,30 @@ T(n) = 2 * T(n/2) + 1 = O(n)
 # assume that the given tree has at least 1 node (root)
 def depth_recursive (root):
     if (root == None):
-        return 0
+        return -1
     
     left = depth_recursive(root.left)
     right = depth_recursive(root.right)
 
     return (1 + max(left, right))
+
+def depth_stack (root):
+    result = -1
+    s = Stack()
+    s.push (tuple([root, -1]))
+
+    while (not s.is_empty()):
+        top = s.top()[0]
+        depth = s.top()[1]
+        s.pop()
+
+        if (top == None):
+            result = max(result, depth)
+        else:
+            s.push(tuple([top.left, depth + 1]))
+            s.push(tuple([top.right, depth + 1]))
+    
+    return result
 
 if __name__ == '__main__':
     print ("Input tree structure:")
@@ -31,8 +51,9 @@ if __name__ == '__main__':
     root.right = TreeNode(2)
     root.left.left = TreeNode(3)
     root.left.right = TreeNode(4)
-    print ("Maximum depth:", depth_recursive(root))
-    print ("=====================")
+    print ("Maximum depth with recursive approach:", depth_recursive(root))
+    print ("Maximum depth with stack approach:", depth_stack(root))
+    print ("==========================================")
     print ("Input tree structure:")
     print ("       0")
     print ("     /   \\")
@@ -43,8 +64,9 @@ if __name__ == '__main__':
     root.left = TreeNode(1)
     root.right = TreeNode(2)
     root.right.right = TreeNode(3)
-    print ("Maximum depth:", depth_recursive(root))
-    print ("=====================")
+    print ("Maximum depth with recursive approach:", depth_recursive(root))
+    print ("Maximum depth with stack approach:", depth_stack(root))
+    print ("==========================================")
     print ("Input tree structure:")
     print ("        1")
     print ("     /    \\")
@@ -61,7 +83,8 @@ if __name__ == '__main__':
     root.right.right = TreeNode(6)
     root.right.right.left = TreeNode(8)
     root.right.left.right = TreeNode(7)
-    print ("Maximum depth:", depth_recursive(root))
+    print ("Maximum depth with recursive approach:", depth_recursive(root))
+    print ("Maximum depth with stack approach:", depth_stack(root))
     
 
 
